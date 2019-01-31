84e916871da4078766cb6652ccf9dbd616080a47
kalanchekaevm <max-kalan@yandex.ru>
2019-02-01 00:26:22 +0300

    Добавлены комментарии в класс SplashScreen

--- AsteroidsGame/SplashScreen.cs
+++ AsteroidsGame/SplashScreen.cs
@@ -12,33 +12,42 @@ namespace AsteroidsGame
       private  static Form startingform = new Form {Width = 500, Height = 500, Text = "Asteroids Game", Icon = new System.Drawing.Icon("Assets/32х32.ico")};
       private  static Button startbutton = new Button {Text = "Начать игру", Width = 120, Height = 50 };
       private  static Button endbutton = new Button {Text = "Выйти из игры", Width = 120, Height = 50 };
+
         
         public  SplashScreen()
         {
-            // Настраиваем стартовый экран
+            // Настраиваем кнопки 
             startbutton.Location = new System.Drawing.Point(200, 200);
             endbutton.Location = new System.Drawing.Point (200, 300);
+            startingform.StartPosition = FormStartPosition.CenterScreen;
 
+            // Добавляем кнопки к форме
             startingform.Controls.Add(startbutton);
             startingform.Controls.Add(endbutton);
 
-            startingform.StartPosition = FormStartPosition.CenterScreen;
+            // Включаем поиск событий 
             startbutton.Click += Startbutton_Click;
             endbutton.Click += Endbutton_Click;
+        }
 
-
+        //Запуск игры
+        public void Run()
+        {
             Application.Run(startingform);
         }
-          
+
         // Настройка кнопки старт
         private void Startbutton_Click(object sender, EventArgs e)
-        { 
+        {
+           
             startingform.Hide();
             Form gameform = new Form { Width = 1080, Height = 720, Text = "Asteroids Game", Icon = new System.Drawing.Icon("Assets/32х32.ico") };
             Game.Init(gameform);
             Game.Draw();
             gameform.ShowDialog();
         }
+
+
         //Настройка кнопки выхода
         private void Endbutton_Click(object sender, EventArgs e)
         {
